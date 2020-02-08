ce6febd98bc63c99933ff0e0c5ab8cd0078d69ab
Philip Laxamana <philip.laxamana@hotmail.com>
2020-02-08 18:40:03 -0500

    login functionality added

--- server.js
+++ server.js
@@ -20,27 +20,6 @@ let port = 3001;
 app.use(express.json());
 app.use(cors());
 
-const database = {
-  users: [
-    {
-      id: '123',
-      name: 'John',
-      email: 'john@gmail.com',
-      password: 'cookies',
-      entries: 0,
-      joined: new Date()
-    },
-    {
-      id: '124',
-      name: 'Sally',
-      email: 'sally@gmail.com',
-      password: 'bananas',
-      entries: 0,
-      joined: new Date()
-    }
-  ]
-}
-
 app.get('/', (req, res) => {
   res.send(database.users);
 })
