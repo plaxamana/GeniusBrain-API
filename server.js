966779b8651fdab6b07c9ea36f80ce9bec761360
Philip Laxamana <philip.laxamana@hotmail.com>
2020-02-08 18:12:15 -0500

    functionality for register endpoint

--- server.js
+++ server.js
@@ -63,24 +63,24 @@ app.post('/register', (req, res) => {
     name: name,
     joined: new Date()
   })
-    .then(user => {
-      res.json(user[0]);
-    })
-    .catch(err => res.status(400).json('Unable to register'))
+  .then(user => {
+    res.json(user[0]);
+  })
+  .catch(err => res.status(400).json('Unable to register'))
 })
 
 app.get('/profile/:id', (req, res) => {
   const { id } = req.params;
   let found = false;
-  database.users.forEach(user => {
-    if(user.id === id) {
-      found = true;
-      return res.json(user);
-    } 
+  db.select('*').from('users').where({id})
+  .then(user => {
+    if(user.length) {
+      res.json(user[0]);
+    } else {
+      res.status(400).json("user not found.")
+    }
   })
-  if (!found) {
-    res.status(400).json("user not found.")
-  }
+  .catch(err => res.status(400).json('Error getting user'))
 });
 
 app.put('/image', (req, res) => {
