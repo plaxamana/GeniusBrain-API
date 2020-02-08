96d6c11674154c4ca2c9974b7f077d300e2ff7ae
Philip Laxamana <philip.laxamana@hotmail.com>
2020-02-08 18:39:39 -0500

    login functionality added

--- server.js
+++ server.js
@@ -46,32 +46,54 @@ app.get('/', (req, res) => {
 })
 
 app.post('/signin', (req, res) => {
-  if(req.body.email === database.users[0].email &&
-    req.body.password === database.users[0].password) {
-      res.json(database.users[0]);
-    } else {
-      res.status(400).json('error logging in');
-    }
+  db.select('email', 'hash').from('login')
+    .where('email', '=', req.body.email)
+    .then(data => {
+      const isValid = bcrypt.compareSync(req.body.password, data[0].hash);
+      if(isValid){
+        return db.select('*').from('users')
+        .where('email', '=', req.body.email)
+        .then(user => {
+          res.json(user[0])
+        })
+        .catch(err => res.status(400).json('unable to get user'))
+      } else {
+        res.status(400).json('wrong credentials')
+      }
+    })
+    .catch( err => res.status(400).json('wrong credentials'));
 })
 
 app.post('/register', (req, res) => {
   const { email, name, password } = req.body;
-  db('users')
-    .returning('*')
-    .insert({
-    email: email,
-    name: name,
-    joined: new Date()
-  })
-  .then(user => {
-    res.json(user[0]);
+  const hash = bcrypt.hashSync(password);
+  db.transaction(trx => {
+    trx.insert({
+      hash: hash,
+      email: email
+    })
+    .into('login')
+    .returning('email')
+    .then(loginEmail => {
+      return trx('users')
+        .returning('*')
+        .insert({
+          email: loginEmail[0],
+          name: name,
+          joined: new Date()
+      })
+      .then(user => {
+        res.json(user[0]);
+      })
+    })
+    .then(trx.commit)
+    .catch(trx.rollback)
   })
   .catch(err => res.status(400).json('Unable to register'))
 })
 
 app.get('/profile/:id', (req, res) => {
   const { id } = req.params;
-  let found = false;
   db.select('*').from('users').where({id})
   .then(user => {
     if(user.length) {
@@ -94,6 +116,11 @@ app.put('/image', (req, res) => {
   })
   .catch(err => res.status(400).json('unable to get entries'));
 })
+
+var hash = bcrypt.hashSync("bacon");
+
+bcrypt.compareSync("bacon", hash); // true
+bcrypt.compareSync("veggies", hash); // false
 
 // bcrypt.hash("bacon", null, null, function(err, hash) {
 //   // Store hash in your password DB.
