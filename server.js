0dcbe22bd271db9448e76b94030b66f9245432ee
Philip Laxamana <philip.laxamana@hotmail.com>
2020-02-08 17:39:58 -0500

    connected postgres database

--- server.js
+++ server.js
@@ -1,6 +1,19 @@
 const express = require('express');
 const bcrypt = require('bcrypt-nodejs');
 const cors = require('cors');
+const knex = require('knex');
+
+const db = knex({
+  client: 'pg',
+  connection: {
+    host : '127.0.0.1',
+    user : 'philip',
+    password : 'admin',
+    database : 'geniusbrain'
+  }
+});
+
+console.log(db.select('*').from('users'));
 
 const app = express();
 let port = 3001;
